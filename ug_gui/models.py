f35cf62f0dbb358b0b97e2294b4e3a1bda11259f
unknown <shardul.vaidya@flame.edu.in>
2018-10-27 14:08:17 +0530

    new house for bhaamti's game

---
+++ ug_gui/models.py
@@ -0,0 +1,72 @@
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+
+
+class Constants(BaseConstants):
+    name_in_url = 'ug_gui'
+    players_per_group = 2
+    num_rounds = 1
+
+    instructions_template = 'ug_gui/Instructions.html'
+
+    endowment = c(10)
+    payoff_if_rejected = c(0)
+    offer_increment = c(1)
+
+    offer_choices = currency_range(0, endowment, offer_increment)
+    offer_choices_count = len(offer_choices)
+    question_correct = c(50)
+
+    keep_give_amounts = []
+    for offer in offer_choices:
+        keep_give_amounts.append((offer, endowment - offer))
+
+class Subsession(BaseSubsession):
+    def creating_session(self):
+        # randomize to treatments
+        for g in self.get_groups():
+            if 'use_strategy_method' in self.session.config:
+                g.use_strategy_method = self.session.config['use_strategy_method']
+            else:
+                g.use_strategy_method = random.choice([True, False])
+
+class Group(BaseGroup):
+    use_strategy_method = models.BooleanField(
+        doc="""Whether this group uses strategy method"""
+    )
+
+    amount_offered = models.CurrencyField(choices=Constants.offer_choices)
+
+    offer_accepted = models.BooleanField(
+        doc="if offered amount is accepted (direct response method)"
+    )
+
+
+    def set_payoffs(self):
+        p1, p2 = self.get_players()
+
+        if self.use_strategy_method:
+            self.offer_accepted = getattr(self, 'response_{}'.format(
+                int(self.amount_offered)))
+
+        if self.offer_accepted:
+            p1.payoff = Constants.endowment - self.amount_offered
+            p2.payoff = self.amount_offered
+        else:
+            p1.payoff = Constants.payoff_if_rejected
+            p2.payoff = Constants.payoff_if_rejected
+
+class Player(BasePlayer):
+    question1 = models.CurrencyField()
+
+    question2 = models.CurrencyField()
+
+    question3 = models.CurrencyField()
+
+    question1_truth = models.BooleanField(initial=False)
+
+    question2_truth = models.BooleanField(initial=False)
+
+    question3_truth = models.BooleanField(initial=False)
